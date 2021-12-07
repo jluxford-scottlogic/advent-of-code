11ab4ae1812256c0e50e7aea543df4d3de610b43
Jamie Luxford <JLuxford@scottlogic.com>
2021-12-07 11:47:43 +0000

    Update 2020 day 9

--- 2020/day9.hs
+++ 2020/day9.hs
@@ -7,26 +7,14 @@ checkInput n as [] = 0
 checkInput n as (b:bs) | not (or [b == x + y | x <- take n as, y <- take n as]) = b
                        | otherwise = checkInput n (b:as) bs
 
-findFailure :: Int -> [Int] -> [Int] -> [Bool]
-findFailure n as [] = []
-findFailure n as (b:bs) = not (or [b == x + y | x <- take n as, y <- take n as]) : findFailure n (b:as) bs
-
 testInput :: Int -> [Int] -> Int
 testInput n as = checkInput n (reverse (take n as)) (drop n as)
 
-testFailure :: Int -> [Int] -> [Bool]
-testFailure n as = [False | n <- [1..25]] ++ findFailure n (reverse (take n as)) (drop n as)
-
-actOnFailure :: Int -> [Int] -> Int
-actOnFailure n as = maximum list * minimum list where
-    failureSet = testFailure n as
-    list = g (f as failureSet)
-    f :: [Int] -> [Bool] -> (Int, [Int])
-    f [] [] = (0,[])
-    f (a:as) (b:bs) | b = (a, [])
-                    | otherwise = fmap (a:) (f as bs)
-    g :: (Int, [Int]) -> [Int]
-    g (sumer, ys) = snd $ head $ filter fst [(sum (take x (drop y ys)) == sumer, take x (drop y ys)) | x <- [1..length ys], y <- [0..length ys - x]]
+findConsecutive :: Int -> [Int] -> Int
+findConsecutive n xs = snd $ head filteredResult where
+    filteredResult = filter (\(a,b) -> a == n) resultSummed
+    resultSummed = fmap (\a -> (sum a, maximum a + minimum a)) allPossible
+    allPossible = [take y (drop x xs) | x <- [0..length xs - 1], y <- [1..length xs - x]]
 
 intListParser :: Parser [Int]
 intListParser = some (read <$> some digit <* many space)
