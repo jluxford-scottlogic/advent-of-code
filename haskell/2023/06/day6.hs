5e936abf03242f44425f112ec84490b79e9cab58
jluxford <jluxford@scottlogic.com>
2023-12-06 10:25:22 +0000

    Add 2023 Haskell day 6

---
+++ haskell/2023/06/day6.hs
@@ -0,0 +1,42 @@
+import Text.Parsec.ByteString
+import Text.Parsec
+import Control.Applicative (some)
+
+type ParsedType = ([Int], [Int])
+
+createValues :: Int -> [Int]
+createValues n = [(n - x) * x | x <- [0..n]]
+
+compareValues :: ([[Int]], [Int]) -> [Int]
+compareValues ([],[]) = []
+compareValues (xs:xss, y:ys) = length (filter (> y) xs) : compareValues (xss, ys)
+
+stage1 :: ParsedType -> Int
+stage1 (xs, ys) = product (compareValues (fmap createValues xs, ys))
+
+combValues :: [Int] -> Int
+combValues ns = read (concat (fmap show ns))
+
+stage2 :: ParsedType -> Int
+stage2 (xs, ys) = length (filter (> (combValues ys)) (createValues (combValues xs)))
+
+runOnInput :: (a -> b) ->  IO (Either ParseError a) -> IO (Either ParseError b)
+runOnInput = fmap . fmap
+
+parsedFile :: String -> IO (Either ParseError ParsedType)
+parsedFile f = parseFromFile usedParser f
+
+parsedTest :: IO (Either ParseError ParsedType)
+parsedTest = parseFromFile usedParser "test.txt"
+
+parsedInput :: IO (Either ParseError ParsedType)
+parsedInput = parseFromFile usedParser "input.txt"
+
+usedParser :: Parser ParsedType
+usedParser = (,) <$ string "Time: " <* spaces <*> parseNumList <* spaces <* string "Distance: " <* spaces <*> parseNumList <* spaces <* eof
+
+parseNumList :: Parser [Int]
+parseNumList = some (parseNum <* spaces)
+
+parseNum :: Parser Int
+parseNum = read <$> (some (oneOf ('-':['0'..'9'])))
