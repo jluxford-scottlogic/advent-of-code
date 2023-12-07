07c3d519a83d6825c52b23775e3ce8534029fd25
jluxford <jluxford@scottlogic.com>
2023-12-07 11:01:53 +0000

    Update 2023 haskell day 7

--- haskell/2023/07/day7.hs
+++ haskell/2023/07/day7.hs
@@ -6,26 +6,24 @@ import Data.List (sort)
 type ParsedType = [Hand]
 
 data Hand = Hand Int [Int] Int
-    deriving (Show)
-
-instance Eq Hand where
-    (Hand _ as _) == (Hand _ bs _) = as == bs
+    deriving (Show, Eq)
 
 instance Ord Hand where
-    (Hand i as _) <= (Hand j bs _) = rank i as < rank j bs || (rank i as == rank j bs && lowerFirst as bs) || as == bs
+    (Hand i as _) <= (Hand j bs _) = ra < rb || (ra == rb && lowerFirst as bs) || as == bs where
+        ra = rank i as
+        rb = rank j bs
 
 rank :: Int -> [Int] -> Int
-rank i as = rankPS i (sort as)
+rank i as = rankPS i (rmvJokers (sort as))
 
 rankPS :: Int -> [Int] -> Int
-rankPS i as | ofAKind (5 - i) noJks = 7
-            | ofAKind (4 - i) noJks = 6
-            | fullHouse i     noJks = 5
-            | ofAKind (3 - i) noJks = 4
-            | twoPair i       noJks = 3
-            | ofAKind (2 - i) noJks = 2
-            | otherwise = 1 where
-                noJks = rmvJokers as
+rankPS i as | ofAKind (5 - i) as = 7
+            | ofAKind (4 - i) as = 6
+            | fullHouse i     as = 5
+            | ofAKind (3 - i) as = 4
+            | twoPair i       as = 3
+            | ofAKind (2 - i) as = 2
+            | otherwise = 1
 
 ofAKind :: Int -> [Int] -> Bool
 ofAKind 0 _ = True
@@ -33,7 +31,6 @@ ofAKind n [] = False
 ofAKind n (a:as) = (length (a:as) >= n && allSame (take n (a:as))) || ofAKind n as
 
 allSame :: [Int] -> Bool
-allSame [] = True
 allSame [b] = True
 allSame (a:b:as) = a == b && allSame (b:as)
 
@@ -43,18 +40,13 @@ rmvJokers (1:as) = rmvJokers as
 rmvJokers (a:as) = a:as
 
 fullHouse :: Int -> [Int] -> Bool
-fullHouse 3 as = True
 fullHouse 2 as = ofAKind 2 as
 fullHouse 1 as = twoPair 0 as || ofAKind 3 as
 fullHouse 0 (a:b:c:d:e:[]) = a == b && c == d && d == e || a == b && b == c && d == e
 
 twoPair :: Int -> [Int] -> Bool
-twoPair 2 as = True
-twoPair 1 as = ofAKind 2 as
-twoPair 0 [] = False
-twoPair 0 [a] = False
-twoPair 0 [a,b,c] = False
 twoPair 0 (a:b:as) = (a == b && ofAKind 2 as) || twoPair 0 (b:as)
+twoPair _ _ = False
 
 lowerFirst :: [Int] -> [Int] -> Bool
 lowerFirst [] [] = True
@@ -62,9 +54,6 @@ lowerFirst (a:as) (b:bs) = a < b || (a == b && lowerFirst as bs)
 
 sumAndInc :: (Int, Int) -> Hand -> (Int, Int)
 sumAndInc (n, s) (Hand _ _ bid) = (n + 1, s + (n * bid))
-
-stage1Test :: ParsedType -> ParsedType
-stage1Test = sort
 
 stage1 :: ParsedType -> Int
 stage1 = snd . (foldl sumAndInc (1,0)) . sort
@@ -107,7 +96,7 @@ parseCard = parseDigit
         <|> 14 <$ char 'A'
 
 parseNum :: Parser Int
-parseNum = read <$> (some (oneOf ('-':['0'..'9'])))
+parseNum = read <$> (some (oneOf ['0'..'9']))
 
 parseDigit :: Parser Int
 parseDigit = (read . (:[])) <$> (oneOf ['0'..'9'])
